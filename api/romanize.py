c9907db5b397aaccff0afd9d93aefaa3d2bb04ac
Takinsen <tandevill90@gmail.com>
2025-05-10 20:36:53 +0700

    change req

--- api/romanize.py
+++ api/romanize.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 from pythainlp.transliterate import romanize
-import deepcut
+from pythainlp.tokenize import word_tokenize
 
 app = Flask(__name__)
 
 @app.route("/romanize", methods=["GET"])
 def romanize_thai():
-    text = request.args.get("text")  # Get ?text=... from query string
+    text = request.args.get("text")
     if not text:
         return jsonify({"error": "Missing 'text' parameter"}), 400
 
-    words = deepcut.tokenize(text)
+    words = word_tokenize(text)
     romanized_words = [romanize(word, engine="thai2rom") for word in words]
     romanized_text = " ".join(romanized_words)
 
