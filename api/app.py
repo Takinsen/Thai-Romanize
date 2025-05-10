e9201604b44014e287c926e51fe4339e69267306
Takinsen <tandevill90@gmail.com>
2025-05-10 20:56:11 +0700

    api

--- api/app.py
+++ api/app.py
@@ -16,5 +16,5 @@ def romanize_thai():
 
     return jsonify({"romanized": romanized_text})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
